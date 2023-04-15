0a1262d3ec3e23605d27f16549e45f80b60929db
Jason K Lai <laijasonk@gmail.com>
2023-04-15 10:18:02 -0500

    Class refactored to allow multiple LLM libraries
    Refactored the main source code so that it contains a base class that
    can be inherited and modified for different types of LLM libraries,
    which will likely require a different set of code to run. Currently
    designed to run on BLOOM, but later Dolly 2.0 may be added.

--- src/Agents.py
+++ src/Agents.py
@@ -6,12 +6,14 @@ os.environ["TRANSFORMERS_CACHE"] = os.path.abspath("./data/models/")
 # Model imports
 import torch
 import transformers
-from transformers import BloomForCausalLM
-from transformers import BloomTokenizerFast
+from transformers import AutoModelForCausalLM
+from transformers import AutoTokenizer
+# from transformers import BloomForCausalLM
+# from transformers import BloomTokenizerFast
 
 
 class ChatBot:
-    """Class to initialize and run AI conversational agent.
+    """Base class to initialize and run AI conversational agents.
 
     Attributes:
         model_name (str): Name of the Hugging Face model
@@ -43,9 +45,7 @@ class ChatBot:
         self.identity = "The following is a conversation between Addie and a human. "
         self.intent = "Addie is helping the human navigate the quality manual. "
         self.behavior = "Addie is conversational, helpful, and friendly. "
-        self.example = (
-            "<line> Human: How are you? <line> Addie: I am doing well, thank you! "
-        )
+        self.example = "<line> Human: How are you? <line> Addie: Good, thank you! "
 
         return None
 
@@ -70,10 +70,10 @@ class ChatBot:
             self.tokenizer_name = tokenizer_name
 
         print(f"Loading model ({self.model_name})")
-        self.model = BloomForCausalLM.from_pretrained(self.model_name)
+        self.model = self._pretrained_model(self.model_name)
 
         print(f"Loading tokenizer ({self.tokenizer_name})")
-        self.tokenizer = BloomTokenizerFast.from_pretrained(self.tokenizer_name)
+        self.tokenizer = self._pretrained_tokenizer(self.tokenizer)
 
         return None
 
@@ -152,6 +152,41 @@ class ChatBot:
 
         return None
 
+    def use_default_prompt(self):
+        """Set the class object to use default prompts.
+
+        Args:
+            None
+        Returns:
+            None
+        """
+
+        pass
+
+    def _pretrained_model(self, model_name):
+        """Private method to load pretrained model.
+
+        Args:
+            model_name (str): Name of the Hugging Face model
+        Returns:
+            model (obj): Pre-trained model
+        """
+
+        #return BloomForCausalLM.from_pretrained(model_name)
+        return AutoModelForCausalLM.from_pretrained(model_name)
+
+    def _pretrained_tokenizer(self, tokenizer_name):
+        """Private method to load pretrained tokenizer.
+
+        Args:
+            tokenizer_name (str): Name of the Hugging Face model
+        Returns:
+            tokenizer (obj): Pre-trained tokenizer
+        """
+
+        #return BloomTokenizerFast.from_pretrained(tokenizer_name)
+        return AutoTokenizer.from_pretrained(tokenizer_name)
+
     def _ai_response(self, prompt):
         """Generate text based on initial prompt.
 
@@ -184,7 +219,7 @@ class ChatBot:
         return updated_prompt
 
     def _parse_prompt(self, prompt, count):
-        """
+        """Format the generated text.
 
         Args:
             prompt (str): Initial prompt to generate text on top of.
@@ -202,3 +237,69 @@ class ChatBot:
         response = response.strip()
 
         return [updated_prompt, response]
+
+
+class BloomBot(ChatBot):
+    # def _pretrained_model(self, model_name):
+    #     return BloomForCausalLM.from_pretrained(model_name)
+
+    # def _pretrained_tokenizer(self, tokenizer_name):
+    #     return BloomTokenizerFast.from_pretrained(tokenizer_name)
+
+    def _use_default_prompt(self):
+        # Prepare chatbot with context
+        context = ""
+        context += (
+            "Hybrid Intelligence is a group that is part of Capgemini Engineering. "
+        )
+        context += "Hybrid Intelligence used to be a company called Tessella. "
+        context += "Hybrid Intelligence is a group that fuses deep human expertise, advanced technology, and intelligent machine capabilities to deliver solutions to clients. "
+        context += "The guidances and policies of Hybrid Intelligence is described in a document called the quality manual. "
+
+        # Add quality manual excerpts if present
+        context += "The following are excerpts from the quality manual of a company called Hybrid Intelligence. "
+        for context_excerpt in glob.glob(os.path.abspath("./data/context/*.txt")):
+            context += "<exerpt> "
+            with open(context_excerpt, "r") as f:
+                context += f.readlines()[0].strip()
+            context += "</exerpt> "
+
+        # Preparing identity prompt
+        identity = ""
+        identity += "The following is a conversation between a company expert from Hybrid Intelligence named Addie and an Employee of Hybrid Intelligence. "
+        identity += "Addie is an AI assistent that answers questions about Hybrid Intelligence's employee policies and guidances, which are described in the staff manual. "
+        identity += "Addie is an AI assistent that answers questions about the best practices for Hybrid Intelligence employees, which are described in the quality manual. "
+
+        # Preparing intent prompt
+        intent = ""
+        intent += "Addie helps the Employee by answering questions about the company policies, guidances, and best practices that are extracted from the quality manual and staff manual. "
+        intent += "When Addie is asked about something unrelated to the company policies, guidances, or best practices, Addie politely apoligizes and responds that she is not designed to answer that question. "
+        intent += "When Addie is asked about specifics about the contents of the quality manual or staff manual, Addie politely apologizes and responds that she does not know the answer yet, because she hasn't read the quality manual or staff manual. "
+
+        # Preparing behavior prompt
+        behavior = ""
+        behavior += "Addie is conservational, helpful, polite, and humanly when responding to the questions from the Employee. "
+        behavior += "The following is a conversation between Addie and an Hybrid Intelligence Employee. "
+
+        # Preparing example prompt
+        example = ""
+        example += "<line> Employee: Hello. How are you? "
+        example += "<line> Addie: Good, thank you for asking! "
+        example += "<line> Employee: Can you tell me about yourself? "
+        example += "<line> Addie: Absolutely! I am Addie, an AI assistent here to answer your questions about the company policies, guidances, and best practices. "
+        example += "<line> Employee: Thank you. Let's start this conversation over. "
+        example += "<line> Addie: Okay, we can restart this conversation now. "
+
+        # Store defaults
+        self.context = context
+        self.identity = identity
+        self.intent = intent
+        self.behavior = behavior
+        self.example = example
+
+        # Set defaults
+        self.set_scenario(
+            self.context, self.identity, self.intent, self.behavior, self.example
+        )
+
+        return None
